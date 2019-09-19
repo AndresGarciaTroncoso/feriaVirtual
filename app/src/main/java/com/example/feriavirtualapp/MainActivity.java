78a50fafef6b95f2163cd102ce9b007ab200549f
aqsalvador <a.quispe@alumnos.duoc.cl>
2019-09-19 14:50:39 -0300

    FeriaVirtual App Android

--- app/src/main/java/com/example/feriavirtualapp/MainActivity.java
+++ app/src/main/java/com/example/feriavirtualapp/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //menu principal frtia virtual
-        // lets go
+        // 1 er commit
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
